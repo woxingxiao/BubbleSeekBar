1a178c415544edfe940a4349cb9ef49c6a9e9b10
woxingxiao <185578499@qq.com>
2016-12-04 15:23:04 +0800

    v1.2 demo updated

--- app/src/main/java/com/xw/samlpe/bubbleseekbar/MainActivity.java
+++ app/src/main/java/com/xw/samlpe/bubbleseekbar/MainActivity.java
@@ -5,11 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.xw.repo.bubbleseekbar.BubbleSeekBar;
+import com.xw.repo.BubbleSeekBar;
 
 import java.util.Random;
 
