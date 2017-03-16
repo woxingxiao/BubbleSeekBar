2479c56c06e44b58a6b93af27ac21dd5b0947004
woxingxiao <chauvet910@gmail.com>
2017-03-16 14:36:00 +0800

    3.0-lite jCenter release

--- bubbleseekbar/src/main/java/com/xw/repo/BubbleSeekBar.java
+++ bubbleseekbar/src/main/java/com/xw/repo/BubbleSeekBar.java
@@ -241,6 +241,9 @@ public class BubbleSeekBar extends View {
         }
         if (isSeekBySection) {
             mPreSecValue = mMin;
+            if (mProgress != mMin) {
+                mPreSecValue = mSectionValue;
+            }
             isShowSectionMark = true;
             isAutoAdjustSectionMark = true;
             isTouchToSeek = false;
